8dce8b2921d24374bcf1c99bffa0e1bcd423e1a3
poilpy <colinpmurphy@aol.com>
2014-12-25 00:00:34 -0800

    Primes

---
+++ primes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () 
+{
+    for (int i=2; i<100; i++) 
+        for (int j=2; j<i; j++)
+        {
+            if (i % j == 0) 
+                break;
+            else if (i == j+1)
+                std::cout << i << "\n";
+        }   
+    return 0;
+}
